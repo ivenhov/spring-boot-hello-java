54badb343929865f259ac712f8b992352f1e471e
ivenhov <iwan.daniel@gmail.com>
2017-03-23 22:54:51 +0000

    Added sample controller

---
+++ src/main/java/com/ivenhov/SampleController.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright Object Matrix 2017
+ */
+
+package com.ivenhov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author daniel
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
